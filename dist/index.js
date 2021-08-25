1a02e63f2f6d32784a210d8c61d18d80faec72ed
ashish-patwal <ashishpatwal147@gmail.com>
2021-08-25 16:15:29 +0530

    new commit;

--- dist/index.js
+++ dist/index.js
@@ -1,13 +1,27 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 const Fetch = require("node-fetch");
-const url = "https://www.youtube.com/c/Fireship";
+const url = "https://www.youtube.com/user/Niccakun";
 const reg = new RegExp('"rssUrl":"https://www.youtube.com/feeds/videos.xml\\?channel_id=\\w+"', 'g');
-const html = async () => {
-    const responce = await Fetch(url);
+const html = () => __awaiter(void 0, void 0, void 0, function* () {
+    const responce = yield Fetch(url);
     switch (responce.status) {
         case 200:
-            const template = await responce.text();
-            console.log(reg.exec(template)[0]);
+            const template = yield responce.text();
+            const rss = reg.exec(template);
+            if (rss != null) {
+                console.log(rss[0]);
+                break;
+            }
+            console.log('Fuck');
             break;
         case 404:
             console.log("Not Found");
@@ -16,5 +30,5 @@ const html = async () => {
             console.log("something other happened");
             break;
     }
-};
+});
 html();
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 const Fetch = require("node-fetch");
-const url: string = "https://www.youtube.com/c/Fireship";
+const url: string = "https://www.youtube.com/user/Niccakun";
 const reg = new RegExp('"rssUrl":"https://www.youtube.com/feeds/videos.xml\\?channel_id=\\w+"', 'g');
 
 const html: Function = async () => {
@@ -8,8 +8,13 @@ const html: Function = async () => {
 
     switch(responce.status) {
         case 200:
-            const template = await responce.text();
-            console.log(reg.exec(template)![0])
+            const template: string = await responce.text();
+            const rss = reg.exec(template);
+            if(rss != null) {
+                console.log(rss[0])
+                break;
+            }
+            console.log('Fuck');
             break;
         case 404:
             console.log("Not Found");
